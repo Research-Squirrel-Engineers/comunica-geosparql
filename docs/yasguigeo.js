f902b5296d26ef594bd32cc7ee6264e1b85d8f77
Timo Homburg <situx@users.noreply.github.com>
2026-04-10 13:16:54 +0200

    Update yasguigeo.js

--- docs/yasguigeo.js
+++ docs/yasguigeo.js
@@ -41185,8 +41185,8 @@ var YasguiGeoTg = (() => {
       } else {
         let result = cellsToMultiPolygon(dggsdict);
         console.log(result);
-        console.log({ "type": "Polygon", "coordinates": result[0] });
-        return { "type": "Polygon", "coordinates": result[0] };
+        console.log({ "type": "MultiPolygon", "coordinates": result });
+        return { "type": "MultiPolygon", "coordinates": result };
       }
     }
     return {};
